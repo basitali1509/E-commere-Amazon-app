676c333f5f14aff4dbfddc1311b2bc428e4cb1d1
Basit Ali <basitali71019@gmail.com>
2023-08-18 13:30:16 +0500

    TextField Size Adjusted

--- lib/common/custom_textfield.dart
+++ lib/common/custom_textfield.dart
@@ -4,34 +4,41 @@ class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
   final int maxLines;
+  final obscureText;
+
   const CustomTextField({
     Key? key,
     required this.controller,
     required this.hintText,
     this.maxLines = 1,
+    this.obscureText = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      decoration: InputDecoration(
-          hintText: hintText,
-          border: const OutlineInputBorder(
-              borderSide: BorderSide(
-            color: Colors.black38,
-          )),
-          enabledBorder: const OutlineInputBorder(
-              borderSide: BorderSide(
-            color: Colors.black38,
-          ))),
-      validator: (val) {
-        if (val == null || val.isEmpty) {
-          return 'Enter your $hintText';
-        }
-        return null;
-      },
-      maxLines: maxLines,
+    return SizedBox(
+      height: maxLines == 1 ? 64 : 110,
+      child: TextFormField(
+        obscureText: obscureText,
+        controller: controller,
+        decoration: InputDecoration(
+            hintText: hintText,
+            border: const OutlineInputBorder(
+                borderSide: BorderSide(
+              color: Colors.black38,
+            )),
+            enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+              color: Colors.black38,
+            ))),
+        validator: (val) {
+          if (val == null || val.isEmpty) {
+            return 'Enter your $hintText';
+          }
+          return null;
+        },
+        maxLines: maxLines,
+      ),
     );
   }
 }
