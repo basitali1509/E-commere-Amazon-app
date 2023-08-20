0075762fa6772972e8297c746b98b7bacb08e667
Basit Ali <basitali71019@gmail.com>
2023-08-20 12:00:51 +0500

    Code Updated

--- lib/widgets/home/deal_of_day.dart
+++ lib/widgets/home/deal_of_day.dart
@@ -1,4 +1,3 @@
-
 import 'package:e_commerce/common/loader.dart';
 import 'package:e_commerce/features/home/service/home_services.dart';
 import 'package:e_commerce/models/product.dart';
@@ -43,74 +42,74 @@ class _DealOfDayState extends State<DealOfDay> {
         : product!.name.isEmpty
             ? const SizedBox()
             : SingleChildScrollView(
-              child: GestureDetector(
-                  onTap: navigateToDetailScreen,
-                  child: Column(
-                    children: [
-                      Container(
-                        alignment: Alignment.topLeft,
-                        padding: const EdgeInsets.only(left: 15, top: 15, bottom: 12),
-                        child: const Text(
-                          'Deal of the day',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ),
-                      Image.network(
-                        product!.images[0],
-                        height: 200,
-                        width: 200,
-                        fit: BoxFit.cover,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.only(left: 15, top: 8),
-                        alignment: Alignment.topLeft,
-                        child: const Text(
-                          '\$100',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      ),
-                      Container(
-                        alignment: Alignment.topLeft,
-                        padding:
-                            const EdgeInsets.only(left: 15, top: 5, right: 40),
-                        child: const Text(
-                          'Basit',
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
+              child: Column(
+                children: [
+                  Container(
+                    alignment: Alignment.topLeft,
+                    padding: const EdgeInsets.only(left: 15, top: 10, bottom: 12),
+                    child: const Text(
+                      'Deal of the day',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  Image.network(
+                    product!.images[0],
+                    height: 200,
+                    width: 250,
+                    fit: BoxFit.cover,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 15, top: 8),
+                    alignment: Alignment.topLeft,
+                    child: const Text(
+                      '\$100',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    padding:
+                        const EdgeInsets.only(left: 15, top: 5, right: 40),
+                    child: const Text(
+                      'Basit',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
 
-                      SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: GestureDetector(
+                      onTap: navigateToDetailScreen,
+                      child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: product!.images
                               .map(
                                 (e) => Image.network(
                                   e,
-                                  fit: BoxFit.fitWidth,
-                                  width: 100,
-                                  height: 100,
-                                ),
-                              )
+                              fit: BoxFit.fill,
+                              width: 200,
+                              height: 120,
+                            ),
+                          )
                               .toList(),
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 15,
-                        ).copyWith(left: 15),
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          'See all deals',
-                          style: TextStyle(
-                            color: Colors.cyan[800],
-                          ),
-                        ),
-                      ),
-                    ],
+                        )
+                    ),
                   ),
-                ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 15,
+                    ).copyWith(left: 15),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'See all deals',
+                      style: TextStyle(
+                        color: Colors.cyan[800],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             );
   }
 }
